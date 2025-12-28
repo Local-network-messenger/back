03541380b2bc8dcb8b0fb283240c3a34abad7d0b
Andrey Kashenin <and.kashenin@mail.ru>
2025-12-28 21:45:18 +0300

    init commit

---
+++ back/src/test/java/com/local_network/back/back/BackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.local_network.back.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
